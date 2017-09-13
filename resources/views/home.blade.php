94ac9ef0d912289137e7612828c6ba630b663e06
kosta_ktg <kosta_ktg@abv.bg>
2017-09-13 19:15:35 +0300

    table bootstrap and delete row ready

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="col-md-8 col-md-offset-2">
+                    <table class="table table-hover">
+
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Registration data</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                  @foreach($users as $user)
+<tr>
+                      <th scope="row">{{ $user->name }}</th>
+                      <td>{{ $user->email }}</td>
+                      <td>{{ $user->created_at }}</td>
+                      <td><a href="{{route('del',$user->id)}}">delete</a></td>
+                  </tr>
+
+                  @endforeach
+                  
+              </tbody>
+          </table>
+      </div>
+
+      <div class="panel-body">
+        @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+        @endif
+
+
+    </div>
+</div>
+</div>
+</div>
+</div>
+@endsection
